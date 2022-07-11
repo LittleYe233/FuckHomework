cce760d99fe47c56238f3079befbd2651fc781b9
LittleYe233 <30514318+LittleYe233@users.noreply.github.com>
2022-07-11 23:10:14 +0800

    build: initialize with SvelteKit
    Initialization command: `npm create svelte FuckHomework`.
    
    Support TypeScript for grammar. Use prettier and ESLint.
    
    Set up Husky to lint commit messages.

---
+++ README.md
@@ -0,0 +1,3 @@
+# FuckHomework
+
+Fuck your received homework from others - blame their formats and manage them, all via a web service.
---
+++ src/routes/index.svelte
@@ -0,0 +1,10 @@
+<script context="module" lang="ts">
+  export const prerender = true;
+</script>
+
+<svelte:head>
+  <title>Home</title>
+  <meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<h1>Homepage</h1>
