b00cdadef870222e3180c9b0cd999874f156a481
LittleYe233 <30514318+LittleYe233@users.noreply.github.com>
2022-07-12 13:37:38 +0800

    chore: set a greyer background and a narrower y-padding

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -31,7 +31,7 @@
   ];
 </script>
 
-<div class="relative flex min-h-screen flex-col overflow-hidden bg-gray-50 px-8 py-6 font-serif sm:py-12">
+<div class="relative flex min-h-screen flex-col overflow-hidden bg-gray-100 px-8 py-6 font-serif sm:py-6">
   <h1 class="my-4 text-center text-3xl font-bold">Homework Submission</h1>
   <div>
     <h2 class="mb-4 text-2xl font-bold">Homework list</h2>
