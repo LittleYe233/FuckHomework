c23c5f88182fb6026e50c4b3508235ff5a195c95
LittleYe233 <30514318+LittleYe233@users.noreply.github.com>
2022-07-14 21:13:28 +0800

    feat: add HomeworkUpload component
    There are still some features to be implemented.

---
+++ src/components/homework_upload.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+</script>
+
+<div class="relative my-4 rounded-2xl bg-white px-8 py-6 ring-1 ring-gray-900/5 transition ease-in-out hover:shadow-xl duration-300">
+  <h3 class="mb-3 text-xl font-bold">Upload homework</h3>
+  <!-- detailed information -->
+  <p>Please upload your homework files and make sure they meet the conditions below:</p>
+  <!-- conditions -->
+  <ul class="mx-4 my-4 list-inside list-disc" />
+  <div class="relative flex flex-col">
+    <input type="file" multiple class="mb-1" />
+    <input
+      type="submit"
+      class="mt-1 text-left rounded-lg border-2 border-gray-100 px-2 py-1 w-fit transition duration-200 ease-in-out hover:shadow-md"
+    />
+  </div>
+</div>
