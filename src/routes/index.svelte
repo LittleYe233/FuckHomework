8b62ee6fa11e50b9b63dbe1175c25a72dcd6af07
LittleYe233 <30514318+LittleYe233@users.noreply.github.com>
2022-07-14 16:50:17 +0800

    chore: read homework list from local configuration

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,34 +1,9 @@
 <script lang="ts">
   import HomeworkSlot from '~/components/homework_slot.svelte';
-  import type { HomeworkMetadata } from '~/lib/types';
+  import type { ProjectConfig } from '~/lib/types';
 
-  const homeworkSlots: HomeworkMetadata[] = [
-    {
-      title: 'Homework 1',
-      content: 'Content 1',
-      semester: '2022 Fall',
-      subject: 'Subject A',
-      chapter: 'Chapter I, Section 1.1',
-      dueTime: new Date('2022-07-12T10:50:28+0800'),
-      submissionMethod: 'Uploading files'
-    },
-    {
-      title: 'Homework 2',
-      content: 'Content 1',
-      subject: 'Subject A',
-      chapter: 'Chapter I, Section 1.1',
-      dueTime: new Date('1145-01-04T11:45:14+0800'),
-      submissionMethod: 'Uploading files'
-    },
-    {
-      title: 'Homework 3',
-      content: 'Content 1',
-      subject: 'Subject A',
-      chapter: 'Chapter I, Section 1.1',
-      dueTime: new Date('1145-01-04T11:45:14+0800'),
-      submissionMethod: 'Uploading files'
-    }
-  ];
+  const cfg: ProjectConfig = __PROJECT_CONFIG__;
+  const homeworkSlots = cfg.assignments ?? [];
 </script>
 
 <div class="relative flex min-h-screen flex-col overflow-hidden bg-gray-100 px-8 py-6 font-serif sm:py-6">
