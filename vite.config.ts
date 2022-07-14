4f9c25da00050fc57a56c7befe800516ea1a86d6
LittleYe233 <30514318+LittleYe233@users.noreply.github.com>
2022-07-14 16:43:01 +0800

    build: rewrite Vite config to TS and add two defined objects
    We rewrite old `vite.config.js` to TypeScript to fit with future TS
    utilities and features.
    
    And we add two defined strings: `__PROJECT_CONFIG__` which is the
    configuration from `config.yml` and `__PROJECT_ROOT__` which represents
    path of the project root.
    
    Please note that directly using these defined objects causes ts(2304)
    error ("Cannot find name"), but the service still works. Other methods
    to type linting are still needed.

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { fileURLToPath } from 'url';
+import { defineConfig } from 'vite';
+import { parseProjectConfig } from './utils/config';
+
+const config = defineConfig(async () => {
+  // read project config
+  /** @note Vanilla Vite supports JSON. Consider replacing YAML. */
+  const cfg = await parseProjectConfig();
+
+  // return user config
+  return {
+    plugins: [sveltekit()],
+    /**
+     * @see https://stackoverflow.com/questions/70648181/how-to-resolve-absolute-path-using-vite-and-eslint-in-svelte
+     */
+    resolve: {
+      alias: {
+        '~': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    define: {
+      __PROJECT_CONFIG__: JSON.stringify(cfg),
+      __PROJECT_ROOT__: `"${__dirname}"`
+    }
+  };
+});
+
+export default config;
