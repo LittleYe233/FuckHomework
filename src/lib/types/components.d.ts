bc6d090e96cd009dbfa79592e9d3ecab818786cd
LittleYe233 <30514318+LittleYe233@users.noreply.github.com>
2022-07-24 21:52:45 +0800

    chore(type): use utility-types to rewrite types
    This commit introduces `DeepRequired<T>` from `utility-types` to rewrite
    types of components and config utilities. And this commit makes the
    types compatible with the current configuration format.

--- src/lib/types/components.d.ts
+++ src/lib/types/components.d.ts
@@ -1,3 +1,5 @@
+import { DeepRequired } from 'utility-types';
+
 /**
  * Homework metadata passed to `HomeworkSlot` svelte component.
  * 
@@ -9,12 +11,16 @@
  * @param dueTime due time of homework (null: no due time)
  * @param submissionMethod method to submit homework
  */
-export interface HomeworkMetadata {
+export interface RawHomeworkMetadata {
   title: string;
   content?: string;
   semester?: string;
   subject?: string;
   chapter?: string;
-  dueTime?: Date | null;
+  dueTime?: string | null;
   submissionMethod?: string;
 }
+export interface HomeworkMetadata extends DeepRequired<RawHomeworkMetadata> {
+  // JSON doesn't directly support `Date` object.
+  dueTime: Date | null;
+};
--- src/lib/types/config.d.ts
+++ src/lib/types/config.d.ts
@@ -1,4 +1,5 @@
-import type { HomeworkMetadata } from './components';
+import type { HomeworkMetadata, RawHomeworkMetadata } from './components';
+import { DeepRequired } from 'utility-types';
 
 /**
  * `server` section of project configuration.
@@ -7,24 +8,40 @@ import type { HomeworkMetadata } from './components';
  * @param port binding port (HTTP)
  * @param strictPort whether to exit if port is already in use, instead of automatically try the next available port
  */
-export interface ProjectServerConfig {
+export interface RawProjectServerConfig {
   host?: string;
   port?: number;
   strictPort?: boolean;
 }
+export type ProjectServerConfig = DeepRequired<RawProjectServerConfig>;
 
 export type AssignmentConfig = HomeworkMetadata;
-export interface RawAssignmentConfig extends AssignmentConfig {
-  // JSON doesn't directly support `Date` object.
-  dueTime?: string | null;
+export type RawAssignmentConfig = RawHomeworkMetadata;
+
+export interface RawHomeworkConfig {
+  savePath?: string,
+  subFolderFormat?: string,
+  entries?: RawAssignmentConfig[];
+}
+export interface HomeworkConfig extends DeepRequired<RawHomeworkConfig> {
+  entries: AssignmentConfig[];
 }
 
-export interface ProjectConfig {
+export interface RawProjectConfig {
   version: number;
-  server?: ProjectServerConfig;
-  assignments?: AssignmentConfig[];
+  server?: RawProjectServerConfig;
+  homework?: RawHomeworkConfig;
+}
+export interface ProjectConfig extends DeepRequired<RawProjectConfig> {
+  homework: HomeworkConfig;
 }
 
-export interface RawProjectConfig extends ProjectConfig {
-  assignments?: RawAssignmentConfig[];
+export interface VarSubstitutions extends Record<string, string> {
+  homeworkTitle: string,
+  /** @note We don't use `content` field because it may be too long. */
+  homeworkSemester: string,
+  homeworkSubject: string,
+  homeworkChapter: string,
+  homeworkDueTime: string,
+  homeworkSubmissionMethod: string
 }
