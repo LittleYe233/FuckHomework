790177ad4a0198827033ddd660d1d8e415670b15
LittleYe233 <30514318+LittleYe233@users.noreply.github.com>
2022-08-04 20:15:37 +0800

    chore(lib.config): rewrite function for split `VarSubstitutions`

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,13 @@
 import lodash from 'lodash';
-import type { AssignmentConfig, ProjectConfig, RawProjectConfig, VarSubstitutions } from './types';
+import type {
+  AssignmentConfig,
+  HomeworkVarSubstitutions,
+  ProjectConfig,
+  RawProjectConfig,
+  StudentInfo,
+  StudentVarSubstitutions,
+  VarSubstitutions
+} from './types';
 import _cfg from '~/../config.json';
 
 /** @note Key is string-type of the version number. */
@@ -79,7 +87,8 @@ export function parseProjectConfig(): ProjectConfig {
 
 export const cfg = parseProjectConfig();
 
-export function getVarSubstitutions(config: ProjectConfig, hw_id: number): VarSubstitutions {
+// for compatibility
+function _getVarSubstitutions_h(config: ProjectConfig, hw_id: number): HomeworkVarSubstitutions {
   const et = config.homework.entries[hw_id];
   return {
     homeworkTitle: et.title,
@@ -91,23 +100,46 @@ export function getVarSubstitutions(config: ProjectConfig, hw_id: number): VarSu
   };
 }
 
+function _getVarSubstitutions_s(student: StudentInfo): StudentVarSubstitutions {
+  return {
+    studentId: student.id.toString(),
+    studentName: student.name
+  };
+}
+
+export function getVarSubstitutions(options: { config?: ProjectConfig; hw_id?: number; student?: StudentInfo }): Partial<VarSubstitutions> {
+  let result = {};
+
+  if (typeof options.config !== 'undefined' && typeof options.hw_id !== 'undefined') {
+    result = { ...result, ..._getVarSubstitutions_h(options.config, options.hw_id) };
+  }
+  if (typeof options.student !== 'undefined') {
+    result = { ...result, ..._getVarSubstitutions_s(options.student) };
+  }
+
+  return result;
+}
+
 /**
  * Parse variables in the given string.
  *
  * @example
- * parseVars("${homeworkTitle}/foo/bar", config, 1) === "Title_of_Homework_with_ID_1/foo/bar"
+ * parseVars("${homeworkTitle}/foo/bar", { config, hw_id: 1 }) === "Title_of_Homework_with_ID_1/foo/bar"
  *
  * @param str the string to be variable-parsed
- * @param config project config
- * @param hw_id homework ID
+ * @param options.config project config
+ * @param options.hw_id homework ID
+ * @param options.student student info
  */
-export function parseVars(str: string, config: ProjectConfig, hw_id: number): string {
-  const varsubs = getVarSubstitutions(config, hw_id);
+export function parseVars(str: string, options: { config?: ProjectConfig; hw_id?: number; student?: StudentInfo }): string {
+  const varsubs = getVarSubstitutions(options);
   const re = new RegExp(
     Object.keys(varsubs)
       .map((v) => '\\${' + v + '}')
       .join('|'),
     'g'
   );
-  return str.replace(re, (matched) => varsubs[matched.slice(2, matched.length - 1)]);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const varsubsFiltered = Object.fromEntries(Object.entries(varsubs).filter(([_, v]) => v != null)) as Record<string, string>;
+  return str.replace(re, (matched) => varsubsFiltered[matched.slice(2, matched.length - 1)]);
 }
