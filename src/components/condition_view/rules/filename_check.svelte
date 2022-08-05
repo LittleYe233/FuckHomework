f5c146159457f2a458943ce3a6a4ce4d7f3a2270
LittleYe233 <30514318+LittleYe233@users.noreply.github.com>
2022-08-05 23:05:35 +0800

    feat(component): add component `FilenameCheck`

---
+++ src/components/condition_view/rules/filename_check.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import type { FilenameCheckAssignmentRuleLoader } from '~/lib/rules';
+
+  export let rule: FilenameCheckAssignmentRuleLoader;
+</script>
+
+<div class="inline-grid">
+  <p>
+    Names of all files should {#if rule.priority === 'whitelist'}<span class="font-bold">be one of</span>{:else}<span>not be any of</span
+      >{/if} the following patterns:
+  </p>
+  <ul class="list-inside list-disc">
+    {#each rule.priority === 'whitelist' ? rule.whitelist : rule.blacklist as li}
+      <li>
+        {@html rule.renderPatternForViewing(li)}
+      </li>
+    {/each}
+  </ul>
+</div>
