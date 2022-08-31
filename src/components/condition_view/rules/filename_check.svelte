e97ed73eaa021835a27ee276724fc0f8a6ef0cd3
LittleYe233 <30514318+LittleYe233@users.noreply.github.com>
2022-08-31 09:36:46 +0800

    chore(component): introduce a new way of substitution
    Now some fields in patterns can be replaced with their actual values.
    For instance, `${homeworkTitle}` will be replaced with `Homework 1`. Now
    only `homework*` properties support this feature.

--- src/components/condition_view/rules/filename_check.svelte
+++ src/components/condition_view/rules/filename_check.svelte
@@ -1,7 +1,40 @@
 <script lang="ts">
+  import { cfg } from '~/lib/config';
   import type { FilenameCheckAssignmentRuleLoader } from '~/lib/rules';
+  import type { AssignmentConfig } from '~/lib/types';
 
   export let rule: FilenameCheckAssignmentRuleLoader;
+  export let hw_id: number = -1;
+
+  let hw: AssignmentConfig | undefined;
+  if (hw_id !== -1) {
+    hw = cfg.homework.entries[hw_id];
+  }
+
+  const renderedPatterns: String[] = [];
+  (rule.priority === 'whitelist' ? rule.whitelist : rule.blacklist).forEach((li) => {
+    let p;
+    if (hw === undefined) {
+      p = rule.renderPatternForViewing(li);
+    } else {
+      p = rule.renderPatternForViewing(li, {
+        homework: hw,
+        varsubs: {
+          homeworkTitle: hw.title,
+          homeworkSemester: hw.semester,
+          homeworkSubject: hw.subject,
+          homeworkChapter: hw.chapter,
+          studentId: '',
+          studentName: ''
+        }
+      });
+    }
+    if (renderedPatterns.includes(p)) {
+      console.debug(`Filename pattern duplicates with others. Rule: ${rule}. Pattern: ${p}.`);
+    } else {
+      renderedPatterns.push(p);
+    }
+  });
 </script>
 
 <div class="inline-grid">
@@ -10,9 +43,9 @@
       >{/if} the following patterns:
   </p>
   <ul class="list-inside list-disc">
-    {#each rule.priority === 'whitelist' ? rule.whitelist : rule.blacklist as li}
+    {#each renderedPatterns as p}
       <li>
-        {@html rule.renderPatternForViewing(li)}
+        {@html p}
       </li>
     {/each}
   </ul>
