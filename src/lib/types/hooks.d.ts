6ddd40f281a30e851f2ac6644c7b5edddb5a857e
LittleYe233 <littleye233@gmail.com>
2022-11-05 21:51:28 +0800

    feat(type): add types for `Error` from `handleError`
    See commit 55317c496b8dfc0953aab433bb8ab92e528b881e.

---
+++ src/lib/types/hooks.d.ts
@@ -0,0 +1,5 @@
+export interface Error {
+  code: string;
+  stack: string;
+  message: string;
+}
--- src/lib/types/index.d.ts
+++ src/lib/types/index.d.ts
@@ -2,3 +2,4 @@ export * from './components';
 export * from './config';
 export * from './rules';
 export * from './lib';
+export * from './hooks';
