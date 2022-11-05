5d967e365efd2cafa0197719c1b08262967c0e77
LittleYe233 <littleye233@gmail.com>
2022-11-05 23:00:40 +0800

    build(vite): add rollup configuration for browserified modules

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,50 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { fileURLToPath } from 'url';
-import { defineConfig } from 'vite';
+import { defineConfig, UserConfig } from 'vite';
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
 
-const config = defineConfig(async () => {
-  return {
-    plugins: [sveltekit()],
-    /**
-     * @see https://stackoverflow.com/questions/70648181/how-to-resolve-absolute-path-using-vite-and-eslint-in-svelte
-     */
-    resolve: {
-      alias: {
-        '~': fileURLToPath(new URL('./src', import.meta.url))
-      }
-    },
-    server: {
-      fs: {
-        strict: false
-      },
-      port: 3000
+/**
+ * @see https://medium.com/@ftaioli/using-node-js-builtin-modules-with-vite-6194737c2cd2
+ */
+const config: UserConfig = {
+  plugins: [sveltekit()],
+  /**
+   * @see https://stackoverflow.com/questions/70648181/how-to-resolve-absolute-path-using-vite-and-eslint-in-svelte
+   */
+  resolve: {
+    alias: {
+      '~': fileURLToPath(new URL('./src', import.meta.url)),
+      events: 'rollup-plugin-node-polyfills/polyfills/events',
+      buffer: 'rollup-plugin-node-polyfills/polyfills/buffer-es6',
+      timers: 'rollup-plugin-node-polyfills/polyfills/timers'
     }
-  };
-});
+  },
+  server: {
+    fs: {
+      strict: false
+    },
+    port: 3000
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis'
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true
+        }),
+        NodeModulesPolyfillPlugin()
+      ]
+    }
+  },
+  build: {
+    rollupOptions: {
+      plugins: [rollupNodePolyFill()]
+    }
+  }
+};
 
 export default config;
